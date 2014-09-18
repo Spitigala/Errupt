9bb48bff4a7f2f5667b0bd818621dfd8d96c9af2
Sahan <keeblersf@gmail.com>
2014-09-17 21:34:46 -0400

    Add user authentication for errorlog controllers tests

--- app/controllers/errorlogs_controller.rb
+++ app/controllers/errorlogs_controller.rb
@@ -1,4 +1,5 @@
 class ErrorlogsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
  		@public_errorlogs = Errorlog.where(public?: true)
   end
