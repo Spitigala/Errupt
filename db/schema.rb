04f94054d457007fa22664f951c525302ea7e054
Sahan <keeblersf@gmail.com>
2014-09-20 21:11:03 -0400

    Add image upload field to errorlog form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140914142853) do
+ActiveRecord::Schema.define(version: 20140921005749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,17 +25,21 @@ ActiveRecord::Schema.define(version: 20140914142853) do
 
   create_table "errorlogs", force: true do |t|
     t.integer  "user_id"
-    t.string   "title",                            null: false
-    t.text     "description",                      null: false
+    t.string   "title",                             null: false
+    t.text     "description",                       null: false
     t.string   "os"
     t.string   "os_version"
     t.string   "language"
     t.string   "language_version"
     t.string   "framework"
     t.string   "framework_version"
-    t.boolean  "public",            default: true
+    t.boolean  "public",             default: true
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "solutions", force: true do |t|
