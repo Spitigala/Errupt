473136c70ec1a6f8b7ab569b340bc916bdc58b53
Sahan <keeblersf@gmail.com>
2014-09-29 01:23:34 -0400

    Fix error where turbolinks was interfering with loading of the chart

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,29 +1,34 @@
-google.load('visualization', '1.0', {'packages':['corechart']});
-
-google.setOnLoadCallback(drawChart);
-
-
 function drawChart() {
 
-    var tagCount = document.getElementById("tag_chart").dataset.tags;
-    var parsedTagCount = JSON.parse(tagCount);
 
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
+    if (document.getElementById("tag_chart") !== null) {
+        var tagCount = document.getElementById("tag_chart").dataset.tags;
+        var parsedTagCount = JSON.parse(tagCount);
 
-    for(var index in parsedTagCount) {
-        data.addRows([
-            [index, parsedTagCount[index]]
-        ]);
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+
+        for(var index in parsedTagCount) {
+            data.addRows([
+                [index, parsedTagCount[index]]
+            ]);
+        }
+
+        var options = {'title':'Errorlog Breakdown',
+                          colors: ['#CF000F', '#9A12B3', '#19B5FE', '#f3b49f', '#f6c7b6'],
+                         'width':375,
+                         'height':300};
+
+
+        var chart = new google.visualization.PieChart(document.getElementById('tag_chart'));
+        chart.draw(data, options);
     }
-
-    var options = {'title':'Errorlog Breakdown',
-                      colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-                     'width':375,
-                     'height':300};
-
-
-    var chart = new google.visualization.PieChart(document.getElementById('tag_chart'));
-    chart.draw(data, options);
 }
+
+function init () {
+    $(document).on('page:change', drawChart);
+    drawChart();
+}
+
+google.load('visualization', '1.0', {'packages':['corechart'], callback: init});
